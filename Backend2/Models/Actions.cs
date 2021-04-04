155576c89b505ec90f9d8ea022d5509eaf48b1c4
spokeepfat007 <Spokeepfat@mail.ru>
2021-04-04 18:30:04 +0700

    Add project files.

---
+++ Backend2/Models/Actions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend2.Models
+{
+    public class Actions
+    {
+        public string val1 { get; set; }
+        public string val2 { get; set; }
+        public string oper { get; set;}
+
+        public string getResult()
+        {
+
+            int a = Int32.Parse(val1);
+            int b = Int32.Parse(val2);
+            switch ((int)Convert.ToChar(oper))
+            {
+                case 42: return (val1) + " * " + (val2) + " = " + (a * b);
+                case 43: return (val1) + " + " + (val2) + " = " + (a + b);
+                case 45: return (val1) + " - " +(val2) + " = " +(a-b);
+                case 47:
+                    try
+                    {
+                        return (val1) + " / " + (val2) + " = " + (a / b);
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        return "Exception: Division by zero.";
+                    }
+                    
+            }
+            return "b";
+        }
+    }
+}
